8b0ceabb4d15ad6eabe48b53c903b67672082f01
David Valderama <david.nvalderrama@gmail.com>
2021-11-11 04:42:23 +0900

    Gitignore and first approach to testing

---
+++ spec/parser_spec.rb
@@ -0,0 +1,14 @@
+class Parser;end
+
+RSpec.describe Parser do
+  describe "#most_visited_pages" do
+    context "with no malformed or missing origins" do
+    end
+    context "with repeated urls and different origins" do
+    end
+    context "with urls with query strings and different origins" do
+    end
+  end
+  describe "#unique_most_visited_pages" do
+  end
+end
