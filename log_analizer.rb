177439cf637ec12ea1aa0d426cfb9a43b12fb266
David Valderama <david.nvalderrama@gmail.com>
2021-11-12 01:59:28 +0900

    First approach on LogAnalizer

--- log_analizer.rb
+++ log_analizer.rb
@@ -4,13 +4,23 @@ class LogAnalizer
 
   def initialize(lines)
     @lines = lines
+    @lines_parsed = Hash.new { |hash, key| hash[key] = [] }
   end
 
   def most_visited_pages
-    puts "aloha"
   end
 
   def unique_most_visited_pages
+  end
+
+  private
+
+  def validate_and_group_lines_by_url
+    lines.each do |line|
+      # match have two groups, url and domain
+      line_parsed = line.gsub("\n", "").scan(/([^\s]+)/).flatten
+      @lines_parsed[line_parsed[0]] << line_parsed[1]
+    end
   end
 
 end
