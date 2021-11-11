01bc0681c392250d27640b63c6579a98897c9828
David Valderama <david.nvalderrama@gmail.com>
2021-11-12 01:02:03 +0900

    Add LogAnalizer class

---
+++ log_analizer.rb
@@ -0,0 +1,16 @@
+class LogAnalizer
+
+  attr_reader :lines
+
+  def initialize(lines)
+    @lines = lines
+  end
+
+  def most_visited_pages
+    puts "aloha"
+  end
+
+  def unique_most_visited_pages
+  end
+
+end
